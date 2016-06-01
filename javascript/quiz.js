e296c4b61db6bd46f19ae98c25d7c64d4241b532
Alex Horton <alexhortonmusic@gmail.com>
2016-06-01 13:13:39 -0500

    finished draft js quiz

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,20 +1,33 @@
 //---------HTML ELEMENTS-----//
 //----------TO CAPTURE-------//
 
-//Get input from user for height of tree
+// Get input from user for height of tree
 var heightInput = document.getElementById('heightInput');
-//Get input from user for character to build tree i.e. "*"
+// Get input from user for character to build tree i.e. "*"
 var charInput = document.getElementById('charInput');
-//"Submit" button for user to click to build tree
+// "Submit" button for user to click to build tree
 var growBtn = document.getElementById('growBtn')
 
 
 
-//---------Functions---//
-function tree (treeBuild) {
+var charString = "";
+var numberChars;
 
+
+// Function to build that tree
+function tree (treeBuild) {
+	numberChars = 1;
+	var spacesForTree = treeBuild.height - 1;
+		// width of tree "box" = (height * 2) - 1
+	for (var i = 0; i < treeBuild.height; i++) {
+		addBlanks(spacesForTree);
+		addChars(treeBuild.char);
+		printTree();
+		spacesForTree--;
+	}
 }
 
+// Function to get input from user, including alert if info not entered
 function submitInput () {
 	if (heightInput.value === "" || charInput.value === "") {
 		alert("You must type a height (number) and a character to build your tree!");
@@ -24,56 +37,42 @@ function submitInput () {
 		var christmas = {
 		height: heightInput.value,
 		char: charInput.value
-		}
+		};
+		tree(christmas);
 	}
 }
 
+// To offset string with blanks
+function addBlanks(x) {
+  for (; x > 0; x--) {
+    charString += " ";
+  }
+}
 
+// To offset string with characters
+function addChars(chosenChar) {
+  for (var i = 1; i <= numberChars; i++) {
+    charString += chosenChar;
+  }
+  numberChars += 2;
+}
 
-
+// Print tree
+function printTree() {
+  console.log(charString);
+  charString = "";
+}
 
 //---------Event Listeners----//
+
+// Function to determine 'enter' keypress
+function pressEnter (event) {
+  var keyPressed = event.keyCode || event.which;
+    if (keyPressed == 13) {
+     	submitInput();
+    }
+};
+
 heightInput.addEventListener('keypress', pressEnter);
 charInput.addEventListener('keypress', pressEnter);
 growBtn.addEventListener('click', submitInput);
-
-
-// var height = document.getElementById('height').value;
-// height = parseInt(height);
-
-// var char = document.getElementById('char').value;
-
-var heightChar = {howTall: height, charUsed: char};
-function tree() {
-	
-	for (var branch in heightChar) {
-		 sum += branch;
-		
-		console.log(branch);
-	}
-}
-
-var forest = tree();
-
-
-// width = (height * 2) - 1
-
-// var grow = document.getElementById('growBtn');
-
-// grow.addEventListener("click", function () {
-// 		for (var i = 0; i < height; i++) {
-// 		branches += [i];
-// 		console.log("branches", branches);
-// 	}
-// })
-
-
-
-// function tree (singleObject) {
-	// var branches = "";
-// 	for (var i = 0; i < height; i++) {
-// 		branches += [i];
-// 		console.log("branches", branches);
-// 	}
-// }
-
